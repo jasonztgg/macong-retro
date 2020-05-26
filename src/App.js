32fc7087a3e9e3c010e10bd67492207d891a9c31
Yisheng Cai <cai.eason@gmail.com>
2020-05-25 17:49:23 -0700

    starter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import firebase from './firebase';
 import './App.css';
 
+const listNames = {
+  WENT_WELL: 'went-well',
+  TO_IMPROVE: 'to-improve',
+  ACTION_ITEMS: 'action-items',
+};
+
 function App() {
+  const [wentWell, setWentWell] = useState([]);
+  const [toImprove, setToImprove] = useState([]);
+  const [actionItems, setActionItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async (listName) => {
+      try {
+        const querySnapshot = await firebase
+          .firestore()
+          .collection('retro-items')
+          .where('list', '==', listNames[listName])
+          .get();
+
+        const dataArray = [];
+
+        await querySnapshot.forEach(async (doc) => {
+          await dataArray.push({
+            ...doc.data(),
+            id: doc.id,
+          });
+        });
+
+        switch (listNames[listName]) {
+          case listNames.WENT_WELL:
+            setWentWell(dataArray);
+            break;
+          case listNames.TO_IMPROVE:
+            setToImprove(dataArray);
+            break;
+          case listNames.ACTION_ITEMS:
+            setActionItems(dataArray);
+            break;
+          default:
+            break;
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData('WENT_WELL');
+    fetchData('TO_IMPROVE');
+    fetchData('ACTION_ITEMS');
+  }, []);
+
+  console.log(wentWell);
+  console.log(toImprove);
+  console.log(actionItems);
+
+  const renderListItem = (item) => <li key={item.id}>{item.content}</li>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <div>Went Well</div>
+        <ul>{wentWell.map(renderListItem)}</ul>
+      </div>
+      <div>
+        <div>To Improve</div>
+        <ul>{toImprove.map(renderListItem)}</ul>
+      </div>
+      <div>
+        <div>Action Items</div>
+        <ul>{actionItems.map(renderListItem)}</ul>
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDcGodUby3zHXIY8sRfhyjOjtiAkE4TnOo',
+  authDomain: 'ztgg-retro-board.firebaseapp.com',
+  databaseURL: 'https://ztgg-retro-board.firebaseio.com',
+  projectId: 'ztgg-retro-board',
+  storageBucket: 'ztgg-retro-board.appspot.com',
+  messagingSenderId: '1031474063216',
+  appId: '1:1031474063216:web:fc071d79acc821978891b5',
+  measurementId: 'G-R743CWPJZ0',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
